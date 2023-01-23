9d596c949c68faf7f85e04e1f1a5c1e30432054e
SQA-SOLOMAN <kavkazus@hotmail.com>
2023-01-23 16:04:27 -0500

    Day1

---
+++ day1_randon_band_generator/day1.py
@@ -0,0 +1,20 @@
+# Basic python inputting, commenting, printing
+# Random Band generator
+
+# 1. Create a greeting for your program.
+
+# 2. Ask the user for the city that they grew up in.
+
+# 3. Ask the user for the name of a pet.
+
+# 4. Combine the name of their city and pet and show them their band name.
+
+# 5. Make sure the input cursor shows on a new line
+
+print(50 * "*" + " Random Band Generator " + 50 * "*")
+
+user_city = input("What's the name of the city you grew up in?:\n")
+
+user_pet = input("What's the name of your favorite pet?:\n")
+
+print('Your Random Band Name is:\n' + user_pet + " " + user_city)
